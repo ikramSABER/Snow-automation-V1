7494a3f415a125b1e5bf49cbd8218956a02da16b
riad el moudden <geams.pc@hotmail.fr>
2025-08-29 04:19:52 +0100

    navigation_TCO updated for automatic assigned_to function

--- libraries/servicenow/demandeInfoOperateur/navigation_TCO.py
+++ libraries/servicenow/demandeInfoOperateur/navigation_TCO.py
@@ -15,6 +15,10 @@ from selenium.common.exceptions import InvalidElementStateException
 def get_driver():
     return BuiltIn().get_library_instance("SeleniumLibrary").driver
 
+def get_wait(timeout=20):
+    driver = get_driver()
+    return WebDriverWait(driver, timeout)
+
 def cliquer_sur_bouton_all():
     from robot.libraries.BuiltIn import BuiltIn
     import time
